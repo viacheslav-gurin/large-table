a6ffb0c8b5e2044f142aab53983740fde63f1041
Viacheslav Gurin <viacheslav.gurin@gmail.com>
2025-05-25 11:03:32 +0500

    Added eslint, prettier and .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <h1>Large Table Project</h1>
-    <div>Table Container</div>
+      <div>Table Container</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
