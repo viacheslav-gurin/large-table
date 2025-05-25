dc8945778ebc0776563eab84290d620f71ef3fb1
Viacheslav Gurin <viacheslav.gurin@gmail.com>
2025-05-25 10:53:48 +0500

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Large Table Project</h1>
+    <div>Table Container</div>
+    </div>
+  )
+}
+
+export default App
